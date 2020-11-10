75564521dcb523d26c3db8a080223695f0af4697
Professor <Professor@WIN-MP3JADQD58A.local>
2020-11-10 17:01:33 +0000

    Update captar.php, captar2.php, and 3 more files...

---
+++ captar2.php
@@ -0,0 +1,20 @@
+<?php
+
+if(isset($_GET['txt_nome']) && isset($_GET['txt_idade'])) {
+	$nome = $_GET['txt_nome'];
+	$idade = $_GET['txt_idade'];
+	echo "Nome:" .$nome. "<br>";
+	echo "Idade:" .$idade;
+}
+else {
+	echo "Erro!!!";
+}
+
+?>
+
+<?php
+
+foreach($_POST as $chave=>$valor)
+	echo $chave. ": " .$valor. "<br>";
+
+?>
